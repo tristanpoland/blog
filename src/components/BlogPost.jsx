599cfd7e7387cb1e71372c42f2d29ac38473995c
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-04-16 16:51:37 -0400

    Update BlogPost.jsx

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -5,7 +5,7 @@ export default function BlogPost({ post }) {
   return (
     <div className="my-8 p-6 bg-white dark:bg-gray-950 rounded-lg shadow-md max-w-4xl center mx-auto">
       <h2 className="text-2xl font-bold mb-2">
-        <Link href={`/blog/${post.slug}`} className="text-blue-600 dark:text-blue-400 hover:underline">
+        <Link href={`/posts/${post.slug}`} className="text-blue-600 dark:text-blue-400 hover:underline">
           {post.title}
         </Link>
       </h2>
